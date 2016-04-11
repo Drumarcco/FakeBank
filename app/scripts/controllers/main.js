affb817efa76b4b1de2c2cb6c8be985d828f7ec4
Marco Alvarado <marcoantonioam9@gmail.com>
2016-04-10 21:41:27 -0700

    Sets angular yeoman project structure

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name fakeBankApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the fakeBankApp
+ */
+angular.module('fakeBankApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
